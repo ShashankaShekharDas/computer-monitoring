7eea14ccc8b044625170cc89a27672ca39499dcc
Shashanka <gudurajasekarreddy@gmail.com>
2023-03-10 00:05:09 +0530

    Fixed Issue with delay between sending records using multithreading

--- computer-monitoring/main.py
+++ computer-monitoring/main.py
@@ -1,16 +1,22 @@
-import datetime
 import json
 import re
+import threading
 from time import sleep
-
+import pythoncom
 from CRUD import create_manage_db
-from commons.Exceptions import LibreException, DBException
+from commons.Exceptions.DBException import DBException
+from commons.Exceptions.LibreException import LibreException
 from data_fetching import get_data_hw
 from initialization import run_libre
 
 
-def get_data_and_put_db(libre_data):
-    # Get Data
+def get_data_from_monitor(result):
+    '''
+        Result variable will store the result from the thread, then the next thread
+        reads the value and then stores in db
+    '''
+    pythoncom.CoInitialize()
+    libre_data = get_data_hw.GetDataHw()
     libre_data.get_monitoring_data()
     json_data = json.loads(libre_data.data_json)
 
@@ -20,31 +26,41 @@ def get_data_and_put_db(libre_data):
             table_name = parameter + identifier.replace("/", "_") + "_" + \
                          json_data[parameter][identifier]["Name"].replace(" ", "")
             table_name = re.sub("[^a-zA-Z0-9 \n]", "_", table_name).strip("_")
-            database_connection.create_table_if_not_exists(table_name)
-            database_connection.insert_data(table_name, json_data[parameter][identifier]["Timestamp"],
-                                            json_data[parameter][identifier]["Value"])
-    print("Inserted Data Successfully")
+            result.append(
+                [
+                    table_name,
+                    [
+                        json_data[parameter][identifier]["Timestamp"],
+                        json_data[parameter][identifier]["Value"]
+                    ]
+                ]
+            )
 
 
-try:
-    while True:
-        '''
-            Bug: On present conditions it takes 11 seconds to write to both Azure and Local DB
-            And 5 second sleep time, so total 16 seconds gap
-            Multithreading?
-        '''
-        time_start = datetime.datetime.now()
-        # initialise objects
-        data_hw = get_data_hw.GetDataHw()
-        database_connection = create_manage_db.ManageDB()
-        # Check pre initialisation environment
-        run_libre.run_libre_if_not_running()
-        get_data_and_put_db(data_hw)
+def database_operations_with_result(result):
+    '''
+        Function gets result from previous func and puts it in db
+    '''
+    database_connection = create_manage_db.ManageDB()
+    run_libre.run_libre_if_not_running()
+    for table in result:
+        table_name, timestamp, value = table[0], table[1][0], table[1][1]
+        database_connection.create_table_if_not_exists(table_name)
+        database_connection.insert_data(table_name, timestamp, value)
+
+
+while True:
+    try:
+        result_get_data_hw = []
+        get_data_thread = threading.Thread(target=get_data_from_monitor, args=(result_get_data_hw,))
+        get_data_thread.start()
+        get_data_thread.join()
+        put_data_db_thread = threading.Thread(target=database_operations_with_result, args=(result_get_data_hw,))
+        put_data_db_thread.start()
         sleep(5)
-
-except DBException.DBException as db_exception:
-    print(db_exception)
-except LibreException.LibreException as libre_exception:
-    print(libre_exception)
-except Exception as e:
-    print(e)
+    except DBException as db_exception:
+        print(db_exception)
+    except LibreException as libre_exception:
+        print(libre_exception)
+    except Exception as exception:
+        print(exception)
